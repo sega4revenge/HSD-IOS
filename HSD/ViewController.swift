b54979624cc961df392e02ce48f3a43f5d45724f
Sega Nightshade <sega4revenge@gmail.com>
2018-05-30 13:37:05 +0700

    Initial Commit

---
+++ HSD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSD
+//
+//  Created by Tô Tử Siêu on 5/30/18.
+//  Copyright © 2018 Finger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
